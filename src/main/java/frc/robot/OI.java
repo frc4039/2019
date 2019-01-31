c30f5accf62f9065ae844b10f747d8cd3c5fab2a
JoshuaUmansky <44981671+JoshuaUmansky@users.noreply.github.com>
2019-01-30 19:40:35 -0500

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -68,10 +68,14 @@ public class OI implements Runnable {
 
 		///////////////////////////////
 		//Drivebase control
-		double x = QuickMaths.normalizeJoystickWithDeadband(driveJoystickThrottle.getRawAxis(Constants.DRIVE_X_AXIS), Constants.kJoystickDeadband);
-		double y = QuickMaths.normalizeJoystickWithDeadband(-driveJoystickThrottle.getRawAxis(Constants.DRIVE_Y_AXIS), Constants.kJoystickDeadband);
+		double x = Math.pow(QuickMaths.normalizeJoystickWithDeadband(driveJoystickThrottle.getRawAxis(Constants.DRIVE_X_AXIS), Constants.kJoystickDeadband), 3);
+		double y = Math.pow(QuickMaths.normalizeJoystickWithDeadband(-driveJoystickThrottle.getRawAxis(Constants.DRIVE_Y_AXIS), Constants.kJoystickDeadband), 3);
+
 
 		driveBaseSubsystem.setDriveOpenLoop(new DriveMotorValues(Util.limit(y + x, 1), Util.limit(y - x, 1)));
+		
+		
+
 		///////////////////////////////
 
 		//TODO: Enable for new drivers maybe
