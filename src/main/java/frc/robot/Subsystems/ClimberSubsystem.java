a2db3abfd17268350152dcd7cc98e2866af4bed4
Matt <eglanfield@gmail.com>
2019-03-29 12:40:19 -0400

    fix climb

--- src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
@@ -383,7 +383,7 @@ public class ClimberSubsystem extends Subsystem {
 
         mClimberDriveMotor.set(ControlMode.PercentOutput, -Constants.kClimbRetractTinyWheelsPercent);
 
-        if (mClimberEncoder.getPosition() > 100) {
+        if (mClimberEncoder.getPosition() > -100) {
             mClimber.setReference(Constants.kClimberRetractSpeed, ControlType.kDutyCycle);
         } else {
             mClimber.setReference(0, ControlType.kDutyCycle);
